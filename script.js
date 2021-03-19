1a3097f03cd52c50b4e36dd7286889c6108e04f5
NathanTempleton <nathan.templeton@ocsbstudent.ca>
2021-03-19 16:15:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+document.getElementById('button-1').addEventListener('click', text1)
+document.getElementById('button-2').addEventListener('click', text2)
+document.getElementById('button-3').addEventListener('click', text3)
+document.getElementById('button-4').addEventListener('click', text4)
+document.getElementById('button-5').addEventListener('click', text5)
+
+function text1 () {
+  document.getElementById('intro').style.display = 'block'
+  document.getElementById('intro-2').style.display = 'block'
+  document.getElementById('job-sum').style.display = 'none'
+  document.getElementById('job-sum-2').style.display = 'none'
+  document.getElementById('job-sum-img').style.display = 'none'
+  document.getElementById('job-sum-3').style.display = 'none'
+  document.getElementById('education').style.display = 'none'
+  document.getElementById('education-2').style.display = 'none'
+  document.getElementById('education-3').style.display = 'none'
+  document.getElementById('education-4').style.display = 'none'
+  document.getElementById('vocation').style.display = 'none'
+  document.getElementById('vocation-2').style.display = 'none'
+  document.getElementById('conclusion').style.display = 'none'
+  document.getElementById('conclusion-2').style.display = 'none'
+}
+
+function text2 () {
+  document.getElementById('job-sum').style.display = 'block'
+  document.getElementById('job-sum-2').style.display = 'block'
+  document.getElementById('job-sum-img').style.display = 'block'
+  document.getElementById('job-sum-3').style.display = 'block'
+  document.getElementById('intro').style.display = 'none'
+  document.getElementById('intro-2').style.display = 'none'
+  document.getElementById('education').style.display = 'none'
+  document.getElementById('education-2').style.display = 'none'
+  document.getElementById('education-3').style.display = 'none'
+  document.getElementById('education-4').style.display = 'none'
+  document.getElementById('vocation').style.display = 'none'
+  document.getElementById('vocation-2').style.display = 'none'
+  document.getElementById('conclusion').style.display = 'none'
+  document.getElementById('conclusion-2').style.display = 'none'
+}
+
+function text3 () {
+  document.getElementById('education').style.display = 'block'
+  document.getElementById('education-2').style.display = 'block'
+  document.getElementById('education-3').style.display = 'block'
+  document.getElementById('education-4').style.display = 'block'
+  document.getElementById('conclusion').style.display = 'none'
+  document.getElementById('conclusion-2').style.display = 'none'
+  document.getElementById('intro').style.display = 'none'
+  document.getElementById('intro-2').style.display = 'none'
+  document.getElementById('job-sum').style.display = 'none'
+  document.getElementById('job-sum-2').style.display = 'none'
+  document.getElementById('job-sum-img').style.display = 'none'
+  document.getElementById('job-sum-3').style.display = 'none'
+  document.getElementById('vocation').style.display = 'none'
+  document.getElementById('vocation-2').style.display = 'none'
+}
+
+function text4 () {
+  document.getElementById('vocation').style.display = 'block'
+  document.getElementById('vocation-2').style.display = 'block'
+  document.getElementById('intro').style.display = 'none'
+  document.getElementById('intro-2').style.display = 'none'
+  document.getElementById('education').style.display = 'none'
+  document.getElementById('education-2').style.display = 'none'
+  document.getElementById('education-3').style.display = 'none'
+  document.getElementById('education-4').style.display = 'none'
+  document.getElementById('job-sum').style.display = 'none'
+  document.getElementById('job-sum-2').style.display = 'none'
+  document.getElementById('job-sum-img').style.display = 'none'
+  document.getElementById('job-sum-3').style.display = 'none'
+  document.getElementById('conclusion').style.display = 'none'
+  document.getElementById('conclusion').style.display = 'none'
+}
+
+function text5 () {
+  document.getElementById('conclusion').style.display = 'block'
+  document.getElementById('conclusion-2').style.display = 'block'
+  document.getElementById('intro').style.display = 'none'
+  document.getElementById('intro-2').style.display = 'none'
+  document.getElementById('job-sum').style.display = 'none'
+  document.getElementById('job-sum-2').style.display = 'none'
+  document.getElementById('job-sum-img').style.display = 'none'
+  document.getElementById('job-sum-3').style.display = 'none'
+  document.getElementById('education').style.display = 'none'
+  document.getElementById('education-2').style.display = 'none'
+  document.getElementById('education-3').style.display = 'none'
+  document.getElementById('education-4').style.display = 'none'
+  document.getElementById('vocation').style.display = 'none'
+  document.getElementById('vocation-2').style.display = 'none'
+}
